34a6b2edf76de885c1642a6c29e481ab22ef7fcb
Selene Moonsilver <bobdablob42+gamedev@gmail.com>
2025-01-26 15:48:13 -0500

    Add map and map back commands

---
+++ commands/cmd_map.go
@@ -0,0 +1,42 @@
+package commands
+
+import (
+	"fmt"
+
+	pokeapi "github.com/snowkittyselene/poke_api"
+)
+
+const locationArea string = baseApiUrl + "/location-area/"
+
+func commandMap(c *Config) error {
+	url := locationArea
+	if c.Next != "" {
+		url = c.Next
+	}
+	res, err := pokeapi.CallAPI(url)
+	if err != nil {
+		return err
+	}
+	c.Next = res.Next
+	c.Previous = res.Previous
+	for _, location := range res.Results {
+		fmt.Println(location.Name)
+	}
+	return nil
+}
+
+func commandMapBack(c *Config) error {
+	if c.Previous == "" {
+		return fmt.Errorf("no previous map page")
+	}
+	res, err := pokeapi.CallAPI(c.Previous)
+	if err != nil {
+		return err
+	}
+	c.Next = res.Next
+	c.Previous = res.Previous
+	for _, location := range res.Results {
+		fmt.Println(location.Name)
+	}
+	return nil
+}
