398d7b3c004c948acad1fa51235afe4d99e80d81
Selene Moonsilver <bobdablob42+gamedev@gmail.com>
2025-01-26 15:55:11 -0500

    Change help to sort commands

--- commands/cmd_help.go
+++ commands/cmd_help.go
@@ -1,12 +1,20 @@
 package commands
 
-import "fmt"
+import (
+	"fmt"
+	"slices"
+)
 
 func commandHelp(c *Config) error {
 	commands := GetCommands()
 	fmt.Println("Welcome to the Pokedex!\nUsage:")
+	cmdNames := []string{}
 	for _, command := range commands {
-		fmt.Printf("%s: %s\n", command.Name, command.Description)
+		cmdNames = append(cmdNames, command.Name)
+	}
+	slices.Sort(cmdNames)
+	for _, cmd := range cmdNames {
+		fmt.Printf("%s: %s\n", commands[cmd].Name, commands[cmd].Description)
 	}
 	return nil
 }
