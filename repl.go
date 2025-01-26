4cd163ffb4a3777e6a1dc3a5c1a46ab2bc1ffab9
Selene Moonsilver <bobdablob42+gamedev@gmail.com>
2025-01-26 02:27:49 -0500

    Add start of REPL functionality

--- repl.go
+++ repl.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"os"
 	"strings"
 )
 
@@ -8,4 +11,19 @@ func cleanInput(text string) []string {
 	lower := strings.ToLower(text)
 	res := strings.Fields(lower)
 	return res
+}
+
+func startRepl() {
+	reader := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("Pokedex > ")
+		reader.Scan()
+		res := cleanInput(reader.Text())
+		if len(res) == 0 {
+			continue
+		}
+		cmd := res[0]
+		fmt.Printf("Your command was: %s\n", cmd)
+	}
 }
