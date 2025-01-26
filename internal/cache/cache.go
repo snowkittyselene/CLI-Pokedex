a22709f7e89f26cd80907ad238d1bd31ee16f701
Selene Moonsilver <bobdablob42+gamedev@gmail.com>
2025-01-26 17:56:01 -0500

    Add cache functionality to reduce API calls

---
+++ internal/cache/cache.go
@@ -0,0 +1,59 @@
+package cache
+
+import (
+	"sync"
+	"time"
+)
+
+type Cache struct {
+	Mu      sync.Mutex
+	Entries map[string]cacheEntry
+}
+
+type cacheEntry struct {
+	createdAt time.Time
+	val       []byte
+}
+
+func NewCache(interval time.Duration) *Cache {
+	cache := Cache{
+		Mu:      sync.Mutex{},
+		Entries: map[string]cacheEntry{},
+	}
+	go cache.reapLoop(interval)
+	return &cache
+}
+
+func (c *Cache) Add(key string, val []byte) {
+	c.Mu.Lock()
+	defer c.Mu.Unlock()
+	c.Entries[key] = cacheEntry{
+		createdAt: time.Now(),
+		val:       val,
+	}
+}
+
+func (c *Cache) Get(key string) ([]byte, bool) {
+	c.Mu.Lock()
+	defer c.Mu.Unlock()
+	val, ok := c.Entries[key]
+	if !ok {
+		return nil, false
+	}
+	return val.val, true
+}
+
+func (c *Cache) reapLoop(interval time.Duration) {
+	ticker := time.NewTicker(interval)
+	defer ticker.Stop()
+	for {
+		t := <-ticker.C
+		c.Mu.Lock()
+		for key, val := range c.Entries {
+			if t.Compare(val.createdAt.Add(interval)) == 1 {
+				delete(c.Entries, key)
+			}
+		}
+		c.Mu.Unlock()
+	}
+}
---
+++ internal/cache/go.mod
@@ -0,0 +1,3 @@
+module github.com/snowkittyselene/cache
+
+go 1.23.5
