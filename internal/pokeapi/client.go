63977644e34bf0d9639c3aa7ca2c091b7f170070
Selene Moonsilver <bobdablob42+gamedev@gmail.com>
2025-01-27 02:09:48 -0500

    Add generic function to reduce reused code

--- internal/pokeapi/client.go
+++ internal/pokeapi/client.go
@@ -1,6 +1,8 @@
 package pokeapi
 
 import (
+	"encoding/json"
+	"io"
 	"net/http"
 	"time"
 
@@ -14,6 +16,10 @@ type Client struct {
 	httpClient http.Client
 }
 
+type APIResponse interface {
+	LocationArea | LocationDetails | PokemonInfo
+}
+
 func NewClient(timeout, interval time.Duration) Client {
 	return Client{
 		cache: pokecache.NewCache(interval),
@@ -21,4 +27,36 @@ func NewClient(timeout, interval time.Duration) Client {
 			Timeout: timeout,
 		},
 	}
+}
+
+func CallAPI[T APIResponse](client *Client, url string, typeEmpty T) (T, error) {
+	response := typeEmpty
+	if val, ok := client.cache.Get(url); ok {
+		if err := json.Unmarshal(val, &response); err != nil {
+			return typeEmpty, err
+		}
+		return response, nil
+	}
+
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return typeEmpty, err
+	}
+
+	res, err := client.httpClient.Do(req)
+	if err != nil {
+		return typeEmpty, err
+	}
+	defer res.Body.Close()
+
+	data, err := io.ReadAll(res.Body)
+	if err != nil {
+		return typeEmpty, err
+	}
+	client.cache.Add(url, data)
+
+	if err := json.Unmarshal(data, &response); err != nil {
+		return typeEmpty, err
+	}
+	return response, nil
 }
